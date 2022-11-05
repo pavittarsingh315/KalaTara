f0a757df6788f7653355f9eacda1facdf2886acc
pavittarsingh315 <pavittarsingh12364@gmail.com>
2022-11-04 20:46:22 -0700

    Added hooks to user model.

--- models/user_model.go
+++ models/user_model.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"errors"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -9,8 +12,23 @@ type User struct {
 	Name      string    `json:"name"`
 	Contact   string    `json:"contact" gorm:"unique"`
 	Password  string    `json:"password"`
+	Role      string    `json:"role"`
 	Strikes   uint8     `json:"strikes"`
 	Birthday  time.Time `json:"birthday"`
 	LastLogin time.Time `json:"lastLogin"`
 	BanTill   time.Time `json:"banTill"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	u.Base.BeforeCreate(tx)
+	// If you implement hooks, you will need to call the respective Base model hook if applicable because it will not fire automatically.
+	// Only implement hooks if you need specific functionality for this model. If not needed, delete the hook and the Base model hooks will fire automatically.
+	return nil
+}
+
+func (u *User) BeforeDelete(tx *gorm.DB) error {
+	if u.Role == "admin" {
+		return errors.New("cannot delete admin user")
+	}
+	return nil
 }
