efc6f1222af9fdfdea358eee171c2de5239a1452
pavittarsingh315 <pavittarsingh12364@gmail.com>
2022-11-29 18:16:41 -0800

    Implemented the search history endpoints.

--- controllers/profile_controllers/search_history_controller.go
+++ controllers/profile_controllers/search_history_controller.go
@@ -1,22 +1,88 @@
 package profilecontrollers
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
+	"nerajima.com/NeraJima/configs"
+	"nerajima.com/NeraJima/models"
 	"nerajima.com/NeraJima/responses"
 )
 
 func AddToSearchHistory(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Add To Search History"}))
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+	reqBody := struct {
+		Query string `json:"query"`
+	}{}
+
+	if err := c.BodyParser(&reqBody); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(responses.NewErrorResponse(fiber.StatusBadRequest, &fiber.Map{"data": "Bad request..."}))
+	}
+
+	// Check if all fields are included
+	if reqBody.Query == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(responses.NewErrorResponse(fiber.StatusBadRequest, &fiber.Map{"data": "Please include all fields."}))
+	}
+
+	reqBody.Query = strings.TrimSpace(reqBody.Query) // remove leading and trailing whitespace
+
+	// Get entire search history
+	var history []models.SearchHistory
+	if err := configs.Database.Model(&reqProfile).Order("search_histories.created_at DESC").Association("SearchHistory").Find(&history); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+	}
+
+	// delete oldest history object
+	historyLen := len(history)
+	if historyLen >= 3 {
+		if err := configs.Database.Model(&models.SearchHistory{}).Delete(&history[historyLen-1]).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+		}
+	}
+
+	newHistoryObj := models.SearchHistory{
+		ProfileId: reqProfile.Id,
+		Query:     reqBody.Query,
+	}
+	if err := configs.Database.Table("search_histories").Create(&newHistoryObj).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Search added to history"}))
 }
 
 func RemoveFromSearchHistory(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Remove From Search History"}))
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Delete the object
+	var historyObj models.SearchHistory
+	if err := configs.Database.Table("search_histories").Delete(&historyObj, "profile_id = ? AND id = ?", reqProfile.Id, c.Params("searchHistoryId")).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Search removed from history."}))
 }
 
 func ClearSearchHistory(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Clear Search History"}))
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Clear associated SearchHistory objects
+	var historyObj models.SearchHistory
+	if err := configs.Database.Table("search_histories").Delete(&historyObj, "profile_id = ?", reqProfile.Id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Search history cleared."}))
 }
 
 func GetSearchHistory(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Get Search History"}))
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Find associated SearchHistory objects
+	var history []models.SearchHistory
+	if err := configs.Database.Model(&reqProfile).Order("search_histories.created_at DESC").Association("SearchHistory").Find(&history); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": history}))
 }
