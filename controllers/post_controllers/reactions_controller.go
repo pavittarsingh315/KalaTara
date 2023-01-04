e3e59f9fee46af45b4acfd998b70a41c150e9f43
pavittarsingh315 <pavittarsingh12364@gmail.com>
2023-01-03 21:20:56 -0800

    Reimplemented the insert and delete operations on post likes, dislikes, and bookmarks.

--- controllers/post_controllers/reactions_controller.go
+++ controllers/post_controllers/reactions_controller.go
@@ -1,23 +1,77 @@
 package postcontrollers
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"nerajima.com/NeraJima/configs"
+	"nerajima.com/NeraJima/models"
 	"nerajima.com/NeraJima/responses"
 )
 
 func LikePost(c *fiber.Ctx) error {
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Delete the dislike object(if it exists)
+	var dislikeObj models.PostDislike
+	if err := configs.Database.Table("post_dislikes").Delete(&dislikeObj, "post_id = ? AND profile_id = ?", c.Params("postId"), reqProfile.Id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	newLikeObj := models.PostLike{
+		PostId:    c.Params("postId"),
+		ProfileId: reqProfile.Id,
+		CreatedAt: time.Now(),
+	}
+	if err := configs.Database.Table("post_likes").Where("post_id = ? AND profile_id = ?", newLikeObj.PostId, newLikeObj.ProfileId).FirstOrCreate(&newLikeObj).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Post has been liked."}))
 }
 
 func DislikePost(c *fiber.Ctx) error {
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Delete the like object(if it exists)
+	var likeObj models.PostLike
+	if err := configs.Database.Table("post_likes").Delete(&likeObj, "post_id = ? AND profile_id = ?", c.Params("postId"), reqProfile.Id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	newDislikeObj := models.PostDislike{
+		PostId:    c.Params("postId"),
+		ProfileId: reqProfile.Id,
+		CreatedAt: time.Now(),
+	}
+	if err := configs.Database.Table("post_dislikes").Where("post_id = ? AND profile_id = ?", newDislikeObj.PostId, newDislikeObj.ProfileId).FirstOrCreate(&newDislikeObj).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Post has been disliked."}))
 }
 
 func RemoveLike(c *fiber.Ctx) error {
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Delete the object
+	var likeObj models.PostLike
+	if err := configs.Database.Table("post_likes").Delete(&likeObj, "post_id = ? AND profile_id = ?", c.Params("postId"), reqProfile.Id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Like has been removed."}))
 }
 
 func RemoveDislike(c *fiber.Ctx) error {
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	// Delete the object
+	var dislikeObj models.PostDislike
+	if err := configs.Database.Table("post_dislikes").Delete(&dislikeObj, "post_id = ? AND profile_id = ?", c.Params("postId"), reqProfile.Id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Dislike has been removed."}))
 }
 
