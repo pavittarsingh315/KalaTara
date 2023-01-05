3402eeb0f2bcdd2ed471433b3348222f285b1890
pavittarsingh315 <pavittarsingh12364@gmail.com>
2023-01-04 18:17:55 -0800

    Completely implemented the route to get bookmarked posts.

--- controllers/post_controllers/bookmark_controller.go
+++ controllers/post_controllers/bookmark_controller.go
@@ -1,6 +1,9 @@
 package postcontrollers
 
 import (
+	"fmt"
+	"math"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -8,6 +11,43 @@ import (
 	"nerajima.com/NeraJima/models"
 	"nerajima.com/NeraJima/responses"
 )
+
+type postsWithoutMedia struct {
+	PostId    string    `json:"post_id"`
+	Title     string    `json:"title" gorm:"column:post_title"`
+	Caption   string    `json:"caption" gorm:"column:post_caption"`
+	CreatedAt time.Time `json:"created_at"`
+
+	ProfileId  string `json:"profile_id"`
+	Username   string `json:"username" gorm:"column:profile_username"`
+	Name       string `json:"name" gorm:"column:profile_name"`
+	MiniAvatar string `json:"mini_avatar" gorm:"column:profile_mini_avatar"`
+
+	MediaUrls string `json:"-" gorm:"column:media_urls"`
+	IsImages  string `json:"-" gorm:"column:is_images"`
+	IsVideos  string `json:"-" gorm:"column:is_videos"`
+	IsAudios  string `json:"-" gorm:"column:is_audios"`
+
+	NumLikes     string `json:"num_likes"`
+	NumDislikes  string `json:"num_dislikes"`
+	NumBookmarks string `json:"num_bookmarks"`
+
+	IsLiked      bool `json:"is_liked"`
+	IsDisliked   bool `json:"is_disliked"`
+	IsBookmarked bool `json:"is_bookmarked"`
+}
+
+type miniPostMedia struct {
+	MediaUrl string `json:"media_url"`
+	IsImage  bool   `json:"is_image"`
+	IsVideo  bool   `json:"is_video"`
+	IsAudio  bool   `json:"is_audio"`
+}
+
+type postsWithMedia struct {
+	postsWithoutMedia
+	Media []miniPostMedia `json:"media"`
+}
 
 func BookmarkPost(c *fiber.Ctx) error {
 	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
@@ -36,6 +76,80 @@ func RemoveBookmark(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Bookmark has been removed."}))
 }
 
+/*
+   b => post_bookmarks
+   p => posts
+   u => profiles
+   m => post_media
+   l => post_likes
+   d => post_dislikes
+*/
+
 func GetBookmarkedPosts(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Get bookmarked posts."}))
+	var page int = c.Locals("page").(int)
+	var limit int = c.Locals("limit").(int)
+	var offset int = c.Locals("offset").(int)
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	query := fmt.Sprintf(
+		"SELECT p.id AS post_id, p.title AS post_title, p.caption AS post_caption, p.created_at AS created_at, "+
+			"u.id AS profile_id, u.username AS profile_username, u.name AS profile_name, u.mini_avatar AS profile_mini_avatar, "+
+			"(SELECT GROUP_CONCAT(m.media_url, '') FROM post_media m WHERE m.post_id = p.id ORDER BY m.position) AS media_urls, "+
+			"(SELECT GROUP_CONCAT(m.is_image, '') FROM post_media m WHERE m.post_id = p.id ORDER BY m.position) AS is_images, "+
+			"(SELECT GROUP_CONCAT(m.is_video, '') FROM post_media m WHERE m.post_id = p.id ORDER BY m.position) AS is_videos, "+
+			"(SELECT GROUP_CONCAT(m.is_audio, '') FROM post_media m WHERE m.post_id = p.id ORDER BY m.position) AS is_audios, "+
+			"COUNT(l.post_id) AS num_likes, COUNT(d.post_id) AS num_dislikes, COUNT(b.post_id) AS num_bookmarks, "+
+			"SUM(CASE WHEN l.profile_id = \"%s\" THEN 1 ELSE 0 END) AS is_liked, "+
+			"SUM(CASE WHEN d.profile_id = \"%s\" THEN 1 ELSE 0 END) AS is_disliked, "+
+			"SUM(CASE WHEN b.profile_id = \"%s\" THEN 1 ELSE 0 END) AS is_bookmarked "+
+			"FROM post_bookmarks b "+
+			"JOIN posts p ON b.post_id = p.id "+
+			"JOIN profiles u ON p.profile_id = u.id "+
+			"LEFT JOIN post_likes l ON p.id = l.post_id "+
+			"LEFT JOIN post_dislikes d ON p.id = d.post_id "+
+			"LEFT JOIN post_bookmarks b2 ON p.id = b2.post_id "+
+			"WHERE b.profile_id = \"%s\" "+
+			"GROUP BY p.id, u.id "+
+			"ORDER BY b.created_at DESC "+
+			"LIMIT %d OFFSET %d",
+		reqProfile.Id, reqProfile.Id, reqProfile.Id, reqProfile.Id, limit, offset,
+	)
+	var unpreparedBookmarkedPosts = []postsWithoutMedia{}
+	if err := configs.Database.Raw(query).Scan(&unpreparedBookmarkedPosts).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	var bookmarkedPosts = []postsWithMedia{}
+	for _, post := range unpreparedBookmarkedPosts { // The limit is capped at 25 and the post_media for a post is capped at 5. This loop has 125 iterations at most
+		var mediaObjs = []miniPostMedia{}
+		mediaUrls := strings.Split(post.MediaUrls, ",")
+		isImages := strings.Split(post.IsImages, ",")
+		isVideos := strings.Split(post.IsVideos, ",")
+		isAudios := strings.Split(post.IsAudios, ",")
+		for i, url := range mediaUrls {
+			mediaObjs = append(mediaObjs, miniPostMedia{MediaUrl: url, IsImage: stringToBool(isImages[i]), IsVideo: stringToBool(isVideos[i]), IsAudio: stringToBool(isAudios[i])})
+		}
+		bookmarkedPosts = append(bookmarkedPosts, postsWithMedia{postsWithoutMedia: post, Media: mediaObjs})
+	}
+
+	var numBookmarks int64
+	if err := configs.Database.Table("post_bookmarks").Where("profile_id = ?", reqProfile.Id).Count(&numBookmarks).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{
+		"data": &fiber.Map{
+			"current_page": page,
+			"last_page":    int(math.Ceil(float64(numBookmarks) / float64(limit))),
+			"data":         bookmarkedPosts,
+		},
+	}))
+}
+
+func stringToBool(s string) bool {
+	if s == "1" {
+		return true
+	} else {
+		return false
+	}
 }
