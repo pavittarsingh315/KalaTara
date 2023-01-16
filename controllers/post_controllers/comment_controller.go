364ce6fb446c3091807e761f72c745ef0e601f45
pavittarsingh315 <pavittarsingh12364@gmail.com>
2023-01-15 17:30:25 -0800

    Implemented the endpoints to get comments on a post and get replies on a comment.

--- controllers/post_controllers/comment_controller.go
+++ controllers/post_controllers/comment_controller.go
@@ -2,6 +2,7 @@ package postcontrollers
 
 import (
 	"fmt"
+	"math"
 	"strings"
 	"time"
 
@@ -169,10 +170,100 @@ func RemoveDislikeFromComment(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Dislike has been removed."}))
 }
 
+type commentResponseObject struct {
+	CommentId string    `json:"comment_id"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+	IsEdited  bool      `json:"is_edited"`
+
+	ProfileId  string `json:"profile_id"`
+	Username   string `json:"username" gorm:"column:profile_username"`
+	MiniAvatar string `json:"mini_avatar" gorm:"column:profile_mini_avatar"`
+
+	NumLikes    int `json:"num_likes"`
+	NumDislikes int `json:"num_dislikes"`
+	NumReplies  int `json:"num_replies"`
+
+	IsLiked    bool `json:"is_liked"`
+	IsDisliked bool `json:"is_disliked"`
+}
+
 func GetComments(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Get Comments."}))
+	var page int = c.Locals("page").(int)
+	var limit int = c.Locals("limit").(int)
+	var offset int = c.Locals("offset").(int)
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	query := fmt.Sprintf(
+		"SELECT c.id AS comment_id, c.body AS body, c.created_at AS created_at, c.is_edited AS is_edited, "+
+			"p.id AS profile_id, p.username AS profile_username, p.mini_avatar AS profile_mini_avatar, "+
+			"l.num_likes, d.num_dislikes, r.num_replies, "+
+			"(CASE WHEN (SELECT profile_id FROM comment_likes WHERE comment_id = c.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_liked, "+
+			"(CASE WHEN (SELECT profile_id FROM comment_dislikes WHERE comment_id = c.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_disliked "+
+			"FROM comments c "+
+			"JOIN profiles p ON p.id = c.commenter_id AND c.comment_replied_to_id IS NULL AND c.post_id = \"%s\" "+
+			"LEFT JOIN (SELECT comment_id, COUNT(*) AS num_likes FROM comment_likes GROUP by comment_id) l ON c.id = l.comment_id "+
+			"LEFT JOIN (SELECT comment_id, COUNT(*) AS num_dislikes FROM comment_dislikes GROUP by comment_id) d ON c.id = d.comment_id "+
+			"LEFT JOIN (SELECT comment_replied_to_id, COUNT(*) AS num_replies FROM comments GROUP by comment_replied_to_id) r ON c.id = r.comment_replied_to_id "+
+			"ORDER BY c.created_at DESC "+
+			"LIMIT %d OFFSET %d",
+		reqProfile.Id, reqProfile.Id, c.Params("postId"), limit, offset,
+	)
+	var comments = []commentResponseObject{}
+	if err := configs.Database.Raw(query).Scan(&comments).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	// Get total number of likes
+	var post = models.Post{Base: models.Base{Id: c.Params("postId")}}
+	numComments := configs.Database.Model(&post).Association("Comments").Count()
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{
+		"data": &fiber.Map{
+			"current_page": page,
+			"last_page":    int(math.Ceil(float64(numComments) / float64(limit))),
+			"data":         comments,
+		},
+	}))
 }
 
 func GetReplies(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{"data": "Get Replies."}))
+	var page int = c.Locals("page").(int)
+	var limit int = c.Locals("limit").(int)
+	var offset int = c.Locals("offset").(int)
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+
+	query := fmt.Sprintf(
+		"SELECT c.id AS comment_id, c.body AS body, c.created_at AS created_at, c.is_edited AS is_edited, "+
+			"p.id AS profile_id, p.username AS profile_username, p.mini_avatar AS profile_mini_avatar, "+
+			"l.num_likes, d.num_dislikes, r.num_replies, "+
+			"(CASE WHEN (SELECT profile_id FROM comment_likes WHERE comment_id = c.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_liked, "+
+			"(CASE WHEN (SELECT profile_id FROM comment_dislikes WHERE comment_id = c.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_disliked "+
+			"FROM comments c "+
+			"JOIN profiles p ON p.id = c.commenter_id AND c.comment_replied_to_id = \"%s\" "+
+			"LEFT JOIN (SELECT comment_id, COUNT(*) AS num_likes FROM comment_likes GROUP by comment_id) l ON c.id = l.comment_id "+
+			"LEFT JOIN (SELECT comment_id, COUNT(*) AS num_dislikes FROM comment_dislikes GROUP by comment_id) d ON c.id = d.comment_id "+
+			"LEFT JOIN (SELECT comment_replied_to_id, COUNT(*) AS num_replies FROM comments GROUP by comment_replied_to_id) r ON c.id = r.comment_replied_to_id "+
+			"ORDER BY c.created_at ASC "+
+			"LIMIT %d OFFSET %d",
+		reqProfile.Id, reqProfile.Id, c.Params("commentId"), limit, offset,
+	)
+	var replies = []commentResponseObject{}
+	if err := configs.Database.Raw(query).Scan(&replies).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	// Get total number of replies
+	var numReplies int64
+	if err := configs.Database.Model(&models.Comment{}).Where("comment_replied_to_id = ?", c.Params("commentId")).Count(&numReplies).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{
+		"data": &fiber.Map{
+			"current_page": page,
+			"last_page":    int(math.Ceil(float64(numReplies) / float64(limit))),
+			"data":         replies,
+		},
+	}))
 }
