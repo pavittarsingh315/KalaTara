f440d8826e6c3d6b90dc4ed89518478d16f1b361
pavittarsingh315 <pavittarsingh12364@gmail.com>
2023-03-30 13:10:52 -0700

    Updated the GetBookmarkedPosts endpoint to use a psql query and overall the endpoint is awesome now.

--- controllers/post_controllers/bookmark_controller.go
+++ controllers/post_controllers/bookmark_controller.go
@@ -1,9 +1,10 @@
 package postcontrollers
 
 import (
-	"fmt"
+	"encoding/json"
 	"math"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -47,6 +48,28 @@ type miniPostMedia struct {
 type postsWithMedia struct {
 	postsWithoutMedia
 	Media []miniPostMedia `json:"media"`
+}
+
+type postResponse struct {
+	PostId    string    `json:"post_id"`
+	Title     string    `json:"title" gorm:"column:post_title"`
+	Caption   string    `json:"caption" gorm:"column:post_caption"`
+	CreatedAt time.Time `json:"created_at"`
+
+	ProfileId  string `json:"profile_id"`
+	Username   string `json:"username" gorm:"column:profile_username"`
+	Name       string `json:"name" gorm:"column:profile_name"`
+	MiniAvatar string `json:"mini_avatar" gorm:"column:profile_mini_avatar"`
+
+	Media json.RawMessage `json:"media" gorm:"column:media_data"`
+
+	NumLikes     int `json:"num_likes"`
+	NumDislikes  int `json:"num_dislikes"`
+	NumBookmarks int `json:"num_bookmarks"`
+
+	IsLiked      bool `json:"is_liked"`
+	IsDisliked   bool `json:"is_disliked"`
+	IsBookmarked bool `json:"is_bookmarked"`
 }
 
 func BookmarkPost(c *fiber.Ctx) error {
@@ -95,35 +118,34 @@ func GetBookmarkedPosts(c *fiber.Ctx) error {
 	var offset int = c.Locals("offset").(int)
 	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
 
-	query := fmt.Sprintf(
-		"SELECT p.id AS post_id, p.title AS post_title, p.caption AS post_caption, p.created_at AS created_at, "+
-			"u.id AS profile_id, u.username AS profile_username, u.name AS profile_name, u.mini_avatar AS profile_mini_avatar, "+
-			"(SELECT GROUP_CONCAT(CONCAT(m.media_url, '') ORDER BY m.position) FROM post_media m WHERE m.post_id = p.id) AS media_urls, "+
-			"(SELECT GROUP_CONCAT(CONCAT(m.is_image, '') ORDER BY m.position) FROM post_media m WHERE m.post_id = p.id) AS is_images, "+
-			"(SELECT GROUP_CONCAT(CONCAT(m.is_video, '') ORDER BY m.position) FROM post_media m WHERE m.post_id = p.id) AS is_videos, "+
-			"(SELECT GROUP_CONCAT(CONCAT(m.is_audio, '') ORDER BY m.position) FROM post_media m WHERE m.post_id = p.id) AS is_audios, "+
-			"l.num_likes, d.num_dislikes, b2.num_bookmarks, "+
-			"(CASE WHEN (SELECT profile_id FROM post_likes WHERE post_id = p.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_liked, "+
-			"(CASE WHEN (SELECT profile_id FROM post_dislikes WHERE post_id = p.id AND profile_id = \"%s\") IS NOT NULL THEN 1 ELSE 0 END) AS is_disliked "+
-			"FROM post_bookmarks b "+
-			"JOIN posts p ON b.post_id = p.id AND b.profile_id = \"%s\" AND p.is_archived = %d "+
-			"JOIN profiles u ON p.profile_id = u.id "+
-			"LEFT JOIN (SELECT post_id, COUNT(*) AS num_likes FROM post_likes GROUP by post_id) l ON p.id = l.post_id "+
-			"LEFT JOIN (SELECT post_id, COUNT(*) AS num_dislikes FROM post_dislikes GROUP by post_id) d ON p.id = d.post_id "+
-			"LEFT JOIN (SELECT post_id, COUNT(*) AS num_bookmarks FROM post_bookmarks GROUP by post_id) b2 ON p.id = b2.post_id "+
-			"GROUP BY p.id, u.id "+
-			"ORDER BY b.created_at DESC "+
-			"LIMIT %d OFFSET %d",
-		reqProfile.Id, reqProfile.Id, reqProfile.Id, 0, limit, offset,
-	)
-	dbCtx, dbCancel := configs.NewQueryContext()
-	defer dbCancel()
-	var unpreparedBookmarkedPosts = []postsWithoutMedia{}
-	if err := configs.Database.WithContext(dbCtx).Raw(query).Scan(&unpreparedBookmarkedPosts).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}, err))
-	}
+	// Run both queries concurrently to reduce response time
+	errChan := make(chan error, 1) // make this buffered so that the goroutine doesn't block
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	var bookmarkedPosts = []postResponse{}
+	go func() {
+		defer wg.Done()
 
-	var bookmarkedPosts = preparePosts(&unpreparedBookmarkedPosts, true, false, false)
+		query := "SELECT profiles.id AS profile_id, profiles.username AS profile_username, profiles.name AS profile_name, profiles.mini_avatar AS profile_mini_avatar, "
+		query += "posts.id AS post_id, posts.title AS post_title, posts.caption AS post_caption, posts.created_at AS created_at, "
+		query += "(SELECT json_agg(json_build_object('media_url', m.media_url, 'is_image', m.is_image, 'is_video', m.is_video, 'is_audio', m.is_audio) ORDER BY m.position) FROM post_media m WHERE m.post_id = posts.id) AS media_data, "
+		query += "(SELECT COUNT(*) FROM post_likes WHERE post_likes.post_id = posts.id) AS num_likes, "
+		query += "(SELECT COUNT(*) FROM post_dislikes WHERE post_dislikes.post_id = posts.id) AS num_dislikes, "
+		query += "(SELECT COUNT(*) FROM post_bookmarks WHERE post_bookmarks.post_id = posts.id) AS num_bookmarks, "
+		query += "EXISTS(SELECT 1 FROM post_likes WHERE post_likes.post_id = posts.id AND post_likes.profile_id = ?) AS is_liked, "
+		query += "EXISTS(SELECT 1 FROM post_dislikes WHERE post_dislikes.post_id = posts.id AND post_dislikes.profile_id = ?) AS is_disliked, "
+		query += "true AS is_bookmarked "
+		query += "FROM posts "
+		query += "JOIN profiles ON posts.profile_id = profiles.id "
+		query += "JOIN post_bookmarks ON post_bookmarks.post_id = posts.id "
+		query += "WHERE post_bookmarks.profile_id = ? AND posts.is_archived = false "
+		query += "ORDER BY post_bookmarks.created_at DESC "
+		query += "LIMIT ? OFFSET ?;"
+
+		dbCtx, dbCancel := configs.NewQueryContext()
+		defer dbCancel()
+		errChan <- configs.Database.WithContext(dbCtx).Raw(query, reqProfile.Id, reqProfile.Id, reqProfile.Id, limit, offset).Scan(&bookmarkedPosts).Error
+	}()
 
 	dbCtx2, dbCancel2 := configs.NewQueryContext()
 	defer dbCancel2()
@@ -131,6 +153,13 @@ func GetBookmarkedPosts(c *fiber.Ctx) error {
 	if err := configs.Database.WithContext(dbCtx2).Table("post_bookmarks").Where("profile_id = ?", reqProfile.Id).Count(&numBookmarks).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}, err))
 	}
+
+	wg.Wait()
+
+	if err := <-errChan; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.NewErrorResponse(fiber.StatusInternalServerError, &fiber.Map{"data": "Unexpected Error. Please try again."}, err))
+	}
+	close(errChan)
 
 	return c.Status(fiber.StatusOK).JSON(responses.NewSuccessResponse(fiber.StatusOK, &fiber.Map{
 		"data": &fiber.Map{
