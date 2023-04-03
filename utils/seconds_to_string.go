7022abfe0903c622a24a6983ac6db93428238472
pavittarsingh315 <pavittarsingh12364@gmail.com>
2023-04-02 21:02:32 -0700

    Refactor the seconds to string method

--- utils/seconds_to_string.go
+++ utils/seconds_to_string.go
@@ -7,34 +7,31 @@ func SecondsToString(seconds int64) string {
 	hours := seconds / 3600
 	days := seconds / (3600 * 24)
 
-	var quantifier string
-	if seconds < 60 {
-		if seconds > 1 {
-			quantifier = "seconds"
-		} else {
-			quantifier = "second"
-		}
-		return fmt.Sprintf("%d %s", seconds, quantifier)
-	} else if minutes < 60 {
-		if minutes > 1 {
-			quantifier = "minutes"
-		} else {
-			quantifier = "minute"
-		}
-		return fmt.Sprintf("%d %s", minutes, quantifier)
-	} else if hours < 24 {
-		if hours > 1 {
-			quantifier = "hours"
-		} else {
-			quantifier = "hour"
-		}
-		return fmt.Sprintf("%d %s", hours, quantifier)
-	} else {
-		if days > 1 {
-			quantifier = "days"
-		} else {
-			quantifier = "day"
-		}
-		return fmt.Sprintf("%d %s", days, quantifier)
+	type TimeUnit struct {
+		Unit     int64
+		Singular string
+		Plural   string
 	}
+
+	// The order of these matters, because we want to display the largest unit first.
+	timeUnits := []TimeUnit{
+		{Unit: days, Singular: "day", Plural: "days"},
+		{Unit: hours, Singular: "hour", Plural: "hours"},
+		{Unit: minutes, Singular: "minute", Plural: "minutes"},
+		{Unit: seconds, Singular: "second", Plural: "seconds"},
+	}
+
+	for _, timeUnit := range timeUnits {
+		if timeUnit.Unit > 0 {
+			var quantifier string
+			if timeUnit.Unit > 1 {
+				quantifier = timeUnit.Plural
+			} else {
+				quantifier = timeUnit.Singular
+			}
+			return fmt.Sprintf("%d %s", timeUnit.Unit, quantifier)
+		}
+	}
+
+	return "0 seconds"
 }
